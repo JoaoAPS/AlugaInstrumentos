34ba84a1e94d0cc47989c34d59b207954801e2c7
João Paludo <jas.paludo@gmail.com>
2021-03-31 14:06:44 -0300

    Cria usuário personalizado

---
+++ backend/users/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.core.exceptions import ValidationError
+
+from users.models import User
+
+
+class UserCreationForm(forms.ModelForm):
+    """Form for creating new users"""
+    password1 = forms.CharField(label='Senha', widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label='Confirme a senha', widget=forms.PasswordInput
+    )
+
+    class Meta:
+        model = User
+        fields = ('email', 'name')
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 != password2:
+            raise ValidationError("As duas senhas devem ser iguais!")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
