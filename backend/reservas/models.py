5ed450d1d83ee9014e4029245c7aad9c004836b0
João Paludo <jas.paludo@gmail.com>
2021-04-05 15:14:54 -0300

    Adiciona campo pedido na model reserva

--- backend/reservas/models.py
+++ backend/reservas/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 
 from equipamentos.models import Equipamento
+from pedidos.models import Pedido
 
 
 class Reserva(models.Model):
     """Recorda que um equipamento está reservado em determinada data"""
 
+    pedido = models.ForeignKey(
+        Pedido, related_name='reservas', on_delete=models.CASCADE
+    )
     equipamento = models.ForeignKey(
         Equipamento, related_name='reservas', on_delete=models.CASCADE
     )
