e14560d8cb5af4350c6a12c92bdbc899db52954b
João Paludo <jas.paludo@gmail.com>
2021-04-08 10:20:25 -0300

    Backend agora usa autenticação por token

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/auth-token/', obtain_auth_token),
     path('api/categorias/', include('categorias.urls')),
     path('api/equipamentos/', include('equipamentos.urls')),
     path('api/pedidos/', include('pedidos.urls')),
