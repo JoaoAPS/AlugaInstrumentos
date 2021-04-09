3522f3dbf29a7c185635270436f894fa386884d1
João Paludo <jas.paludo@gmail.com>
2021-04-09 09:54:46 -0300

    Backend: view de login retorna informações do user além do token

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.authtoken.views import obtain_auth_token
 
+from users.views import AuthTokenView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth-token/', obtain_auth_token),
+    path('api/login/', AuthTokenView.as_view(), name='auth-token'),
     path('api/categorias/', include('categorias.urls')),
     path('api/equipamentos/', include('equipamentos.urls')),
     path('api/pedidos/', include('pedidos.urls')),
--- backend/users/tests.py
+++ backend/users/tests.py
@@ -1,3 +1,37 @@
-from django.test import TestCase
+import pytest
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from rest_framework.test import APIClient
+from rest_framework import status
 
-# Create your tests here.
+
+@pytest.mark.django_db
+def test_auth_token_view_successful():
+    user = get_user_model().objects.create_user(
+        email='test@mm.com', name='Testo', password='123test'
+    )
+
+    url = reverse('auth-token')
+    res = APIClient().post(
+        url, {'username': 'test@mm.com', 'password': '123test'}
+    )
+
+    assert res.status_code == status.HTTP_200_OK
+    assert res.data['email'] == user.email
+    assert res.data['id'] == user.id
+    assert 'token' in res.data.keys()
+
+
+@pytest.mark.django_db
+def test_auth_token_view_unsuccessful():
+    get_user_model().objects.create_user(
+        email='test@mm.com', name='Testo', password='123test'
+    )
+
+    url = reverse('auth-token')
+    res = APIClient().post(
+        url, {'username': 'test@mm.com', 'password': 'wrong_pass'}
+    )
+
+    assert res.status_code == status.HTTP_400_BAD_REQUEST
+    assert 'token' not in res.data.keys()
